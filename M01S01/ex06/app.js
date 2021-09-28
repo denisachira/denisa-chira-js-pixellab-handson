435462795fda3d26dbca6bc0879937aa4d696430
Denisa <84805487+denisachira@users.noreply.github.com>
2021-09-28 09:38:51 +0300

    JS01 Ex03-06

---
+++ M01S01/ex06/app.js
@@ -0,0 +1,14 @@
+var inputRadius = document.getElementById('radius');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  var radius = inputRadius.value || 0;
+  var result = 0;
+
+  result = 4 * Math.PI * radius * radius;
+
+  elementResult.innerText = result;
+
+  event.preventDefault();
+});
