9bf4951392950ae51734eb6fd0f3efe305c99103
Denisa <84805487+denisachira@users.noreply.github.com>
2021-11-03 14:41:32 +0200

    la clasa

---
+++ M01S04/ex12/app.js
@@ -0,0 +1,11 @@
+var globalVariable = 'Ma aflu in contextul global';
+
+function contextTest() {
+  var localVariable = 'Ma aflu in contextul global';
+  var globalVariable = 'Nu ma aflu in contextul global';
+
+  console.log(globalVariable);
+  console.log(localVariable);
+}
+
+contextTest();
